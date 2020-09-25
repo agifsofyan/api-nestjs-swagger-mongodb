0326b8fa2137e33a3d30da0b4b3801858d1b2ee8
Agung Maulana <iamaul@hotmail.com>
2020-09-25 17:12:52 +0700

    update xendit service

--- src/xendit/xendit.service.ts
+++ src/xendit/xendit.service.ts
@@ -15,7 +15,7 @@ export class XenditService {
     Promise<{ error: string; data: any; cart: any; user: IUser; }> {
         const xendit = new Xendit({ secretKey: XENDIT_SECRET_KEY });
         const { Invoice } = xendit;
-        const issue = new Invoice();
+        const issue = Invoice({});
 
         const { cart } = session;
         const cartItem = prepareCart(cart);
@@ -30,6 +30,7 @@ export class XenditService {
                 currency: 'IDR',
                 reminder_time: 1
             });
+            console.log(res);
             return { error: null, data: res, cart, user }
         } catch (error) {
             console.log(error.message);
