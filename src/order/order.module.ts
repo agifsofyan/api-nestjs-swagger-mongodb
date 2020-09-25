9116f507826fbb8e0b0628ba93431706e939435b
Agung Maulana <iamaul@hotmail.com>
2020-09-25 15:17:04 +0700

    update order module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { XenditModule } from '../xendit/xendit.module';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { OrderSchema } from './schemas/order.schema';
@@ -9,7 +10,8 @@ import { OrderSchema } from './schemas/order.schema';
   imports: [
     MongooseModule.forFeature([
       { name: 'Order', schema: OrderSchema }
-    ])
+    ]),
+    XenditModule
   ],
   controllers: [OrderController],
   providers: [OrderService]
--- src/xendit/xendit.module.ts
+++ src/xendit/xendit.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { XenditService } from './xendit.service';
 
 @Module({
-  providers: [XenditService],
-  exports: [MongooseModule]
+  providers: [XenditService]
 })
 export class XenditModule {}
