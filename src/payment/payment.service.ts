09a0df2c024239edeea38279039ac5658002f8e4
agif <alpenizme@gmail.com>
2020-11-11 04:27:11 +0700

    Merge conflict

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -172,16 +172,9 @@ export class PaymentService {
         }
     }
 
-<<<<<<< HEAD
     async callback(payment: any){
 	const { method, external_id, pay_uid } = payment
 	const { name, info} = method
-=======
-    async callback(payment){
-        const { method, status, external_id, payment_code, pay_uid } = payment
-        const payment_type = await this.paService.getMethod(method)
-        const { name, info } = payment_type
-
         var url
         if(info === 'Virtual-Account'){
             url = `${baseUrl}/callback_virtual_account_payments/payment_id=${pay_uid}`
