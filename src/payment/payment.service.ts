ead070b28a29e3968b4597f236d3186226df59c7
agif <alpenizme@gmail.com>
2020-11-11 07:30:59 +0700

    adding library

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -163,8 +163,8 @@ export class PaymentService {
         }
     }
 
-    /**
     async callback(payment: any){
+        // console.log('payment', payment)
 	const { method, external_id, pay_uid } = payment
 	const { name, info} = method
         var url
