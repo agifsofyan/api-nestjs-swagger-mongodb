d112a7d20ce8caec82af10d12f5e62f5b2831911
figa-Oz <alpenizme@gmail.com>
2021-04-05 17:02:51 +0700

    profile return III

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -36,39 +36,97 @@ export class CommentService {
     }
 
     async likeComment(comment_id: string, user: any) {
+        var ID = comment_id
         const like:any = { liked_by: user._id }
+        var react = false
+        var comment:any = await this.commentModel.findById(comment_id)
 
-        var comment = await this.commentModel.findById(comment_id)
-
-        // if(!comment) throw new NotFoundException('comment not found');
         if(!comment){
-            comment = await this.commentModel.findOne({'reactions.$._id': comment_id})
-            console.log('comment reaction', comment)
+            react = true
+            comment = await this.commentModel.findOne({'reactions._id': comment_id}).then((val:any) => {
+                ID = val._id
+                if(val){
+                    val = val.reactions.filter(res => res._id == comment_id)
+                    return val[0]
+                }
+            })
         }
-        
-        const commentFind = comment.likes.find(val => val.liked_by == user._id.toString())
 
-        if(commentFind){
-            return { msg: 'already like this comment', comment }
+        if(!comment) throw new NotFoundException('comment not found');
+
+        const reactions = comment.likes.filter((val) => {
+            if(val){
+                return val.liked_by == user._id.toString()
+            }
+        })
+
+        var msg = 'already like this comment'
+        const likes: any = { liked_by: user._id }
+
+        if(react){
+            if(reactions.length == 0) {   
+                await this.commentModel.findOneAndUpdate(
+                    { 'reactions._id': comment_id },
+                    { $push: { 'reactions.$.likes': likes } },
+                )
+                
+                msg = 'like this comment sucessfuly'
+            }
         }else{
-            comment.likes.push(like)
-            await comment.save()
-
-            return { msg: 'like this comment sucessfuly', comment: await this.commentModel.findById(comment_id) }
+            if(reactions.length == 0) {
+                comment.likes.push(like)
+                await comment.save()
+                
+                msg = 'like this comment sucessfuly'
+            }
         }
+
+        return { msg: msg, comment: await this.commentModel.findById(ID) }
     }
 
     async replyComment(comment_id: string, input: any, user: any) {
         input.user = user._id
         input.created_at = new Date()
 
-        const comment = await this.commentModel.findById(comment_id)
+        var ID = comment_id
+        var react = false
+        var comment:any = await this.commentModel.findById(comment_id)
+
+        console.log('comment-0',comment)
+
+        if(!comment){
+            react = true
+            comment = await this.commentModel.findOne({'reactions._id': comment_id}).then((val:any) => {
+                if(val) ID = val._id;
+            })
+        }
 
         if(!comment) throw new NotFoundException('comment not found');
 
-        comment.reactions.push(input)
-        await comment.save()
+        console.log('comment-2',comment)
 
-        return await this.commentModel.findById(comment_id)
+        var msg = 'already like this comment'
+        const reply: any = {
+            _id: new ObjectId(),
+            user: user._id,
+            comment: input.comment,
+            created_at: new Date()
+        }
+
+        if(react){
+            await this.commentModel.findOneAndUpdate(
+                { 'reactions._id': comment_id },
+                { $push: { 'reactions.$.reactions': reply } },
+            )
+            
+            msg = 'reply this comment sucessfuly'
+        }else{
+            comment.reactions.push(reply)
+            await comment.save()
+            
+            msg = 'reply this comment sucessfuly'
+        }
+
+        return await this.commentModel.findById(ID)
     }
 }
