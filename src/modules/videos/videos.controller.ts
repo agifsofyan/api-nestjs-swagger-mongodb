79e6da28ec896bfa49c5c5f2a27fffa2c78f7e18
figa-Oz <alpenizme@gmail.com>
2021-04-15 07:57:42 +0700

    adding videos like

--- src/modules/videos/videos.controller.ts
+++ src/modules/videos/videos.controller.ts
@@ -26,14 +26,98 @@ import { JwtGuard } from '../auth/guards/jwt.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { VideosService } from './videos.service';
+import { MediaType } from '../userproducts/dto/userproducts.dto';
+import { UserproductsService } from '../userproducts/userproducts.service';
 
 var inRole = ["USER"];
 
 @ApiTags("Videos")
 @UseGuards(RolesGuard)
-@Controller('videos')
+@Controller('userproducts/videos')
 export class VideosController {
-    constructor(private videoService: VideosService) {}
+    constructor(
+        private videoService: VideosService,
+        private readonly userproductsService: UserproductsService
+    ) {}
+
+    /**
+	 * @route    Get /api/v1/userproducts/videos
+	 * @desc     Media List
+	 * @access   Public
+	*/
+	@Get()
+	@UseGuards(JwtGuard)
+	@Roles("USER")
+	@ApiBearerAuth()
+	@ApiOperation({ summary: 'Media List | Client' })
+
+	@ApiQuery({
+		name: 'type',
+		description: "Type Media",
+		required: true,
+		explode: true,
+		type: String,
+		enum: MediaType,
+		isArray: false
+	})
+
+	@ApiQuery({
+		name: 'index',
+		description: "Available to all media or only first index",
+		required: false,
+		explode: true,
+		type: Boolean,
+		isArray: false
+	})
+
+	async videoList(
+		@Req() req,
+		@Res() res,
+		@Query('type') type: string,
+		@Query('index') index: boolean,
+	)  {
+		const user = req.user
+		const result = await this.userproductsService.mediaList(user, type, index);
+		return res.status(HttpStatus.OK).json({
+			statusCode: HttpStatus.OK,
+			message: `success get data of ${type}`,
+			total: result.length,
+			data: result
+		});
+	}
+
+    /**
+	 * @route    Get /api/v1/userproducts/videos/product_id/:product_id
+	 * @desc     Media List
+	 * @access   Public
+	*/
+	@Get('product_id/:product_id')
+	@UseGuards(JwtGuard)
+	@Roles("USER")
+	@ApiBearerAuth()
+	@ApiOperation({ summary: 'Media List | Client' })
+
+	@ApiParam({
+		name: 'product_id',
+		description: "Product ID",
+		required: true,
+		explode: true,
+		type: String,
+		example: '602dda671e352b12bc226dfd'
+	})
+
+	async videoSeries(
+		@Param('product_id') product_id: string,
+		@Res() res,
+	)  {
+		const result = await this.userproductsService.videoSeries(product_id);
+		return res.status(HttpStatus.OK).json({
+			statusCode: HttpStatus.OK,
+			message: `success get data videos`,
+			total: result.length,
+			data: result
+		});
+	}
 
     /**
 	 * @route   POST api/v1/videos/:video_id/like
--- src/modules/videos/videos.module.ts
+++ src/modules/videos/videos.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VideoSchema } from './schemas/videos.schema';
 import { ContentModule } from '../content/content.module';
 import { ProfileModule } from '../profile/profile.module';
+import { UserproductsModule } from '../userproducts/userproducts.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ProfileModule } from '../profile/profile.module';
     ]),
     forwardRef(() => ContentModule),
     ProfileModule,
+    forwardRef(() => UserproductsModule)
   ],
   providers: [VideosService],
   controllers: [VideosController],
