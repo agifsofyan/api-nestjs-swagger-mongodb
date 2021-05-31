def268347a512595baaf40231be63a5d3e94aca6
Agif Sofyan <alpenizme@gmail.com>
2021-05-31 16:15:21 +0700

    post list order by desc

--- src/modules/content/fulfillment/interfaces/fulfillment.interface.ts
+++ src/modules/content/fulfillment/interfaces/fulfillment.interface.ts
@@ -26,6 +26,7 @@ export interface IPost extends Document {
      video: string;
      tips: string;
      author: string;
+     postdate: Date;
 }
 
 export interface IFulfillment extends Document {
